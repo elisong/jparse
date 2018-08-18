f2b234c7b74d2dc67b13ff3e31b37ca2e3bab11a
Eli Song <elisong.ah@gmail.com>
2018-08-18 18:35:51 +0800

    Remove collections.defaultdict

--- tests/test_flatten_map.py
+++ tests/test_flatten_map.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict
 from jparse import JParser
 
 jp = JParser()
@@ -15,19 +15,19 @@ TEST_CASE2 = OrderedDict([('A1', [OrderedDict([('B1', 4), ('B2', 5)]),
 
 def test_flatten_map_default():
     result1 = jp.flatten_map(TEST_CASE2)
-    expected1 = defaultdict(None, {'A1': [{'B1': 4, 'B2': 5},
-                                          {'B1': 6, 'B3': 7}],
-                                   'A2_C1': [8, 9], 'A2_C2': [10, 11],
-                                   'A3_A4': 10, 'A3_A1_B4': 12})
+    expected1 = {'A1': [{'B1': 4, 'B2': 5},
+                        {'B1': 6, 'B3': 7}],
+                 'A2_C1': [8, 9], 'A2_C2': [10, 11],
+                 'A3_A4': 10, 'A3_A1_B4': 12}
     assert result1 == expected1
 
 
 def test_flatten_map_with_prefix():
     result1 = jp.flatten_map(TEST_CASE2, prefix='F')
-    expected1 = defaultdict(None, {'F_A1': [{'B1': 4, 'B2': 5},
-                                            {'B1': 6, 'B3': 7}],
-                                   'F_A2_C1': [8, 9], 'F_A2_C2': [10, 11],
-                                   'F_A3_A4': 10, 'F_A3_A1_B4': 12})
+    expected1 = {'F_A1': [{'B1': 4, 'B2': 5},
+                          {'B1': 6, 'B3': 7}],
+                 'F_A2_C1': [8, 9], 'F_A2_C2': [10, 11],
+                 'F_A3_A4': 10, 'F_A3_A1_B4': 12}
     assert result1 == expected1
 
 
